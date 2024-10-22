0046c727c9c621c96333e15546cca1519a399fc0
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:17:26 +0200

    commit terzo

--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -13,7 +13,10 @@ public class MioThread extends Thread{
     private String numeroDaIndovinareString;
     private int numeroDaIndovinareInt;
     private String risposta;
+    private String continuo;
     
+    int numeroTentativi =0; 
+
     public MioThread(Socket socket, int numeroCasuale) {
             this.socket = socket;
             this.numeroCasuale = numeroCasuale;
@@ -25,42 +28,62 @@ public class MioThread extends Thread{
         try {
             String nuemroDaIndovinare;
             String sceltaInput;
-            
+            String scelta ="";
+            int numeroTentativi =0; 
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            
-            do {             
+           //do{
+            do {   
+               
+             
+                        
                 System.out.println(numeroCasuale);
                // prendo una stringa in imput, e poi cambio e lo rendo intero
                 numeroDaIndovinareString = in.readLine();
-                numeroDaIndovinareInt = Integer.parseInt(numeroDaIndovinareString);
+               
+                if(numeroDaIndovinareString.equals("")){
+                    out.writeBytes("!!!" );
+                   
+                }else{
+                    numeroDaIndovinareInt = Integer.parseInt(numeroDaIndovinareString);
+                }
+                
+                if( numeroDaIndovinareInt< 0 || numeroDaIndovinareInt >100 ){
+                    out.writeBytes("!!!" );
+                    System.out.println("!!!");
+                }
                 System.out.println(numeroDaIndovinareInt);
 
+
+
+                //controlli 
                 if(numeroDaIndovinareInt > numeroCasuale){
                    
                     out.writeBytes(">"+ "\n");
                 }
+
                 if( numeroDaIndovinareInt< numeroCasuale){
-                    out.writeBytes(risposta + "\n");
+                    out.writeBytes("<" + "\n");
                 }
+                
+                //indovinato, invio numero tentativi
+                if( numeroDaIndovinareInt == numeroCasuale){
+                    out.writeBytes("=" + "\n");
+                    String numeroTentativiString = Integer.toString(numeroTentativi + 1);
+                   out.writeBytes(numeroTentativiString );
+                    break;
+                }
+                
+                numeroTentativi ++ ; 
+                System.out.println("numero tentativi :" + numeroTentativi);
 
+            } while (true); 
 
-
-
-            } while (true);
-
-
-
-
-
-
-
-
+           //}while(scelta.equals("y"));
+                  
+              socket.close();
 
             
-
-
-            //socket.close();
             
         } catch (IOException e) {
             // TODO Auto-generated catch block
